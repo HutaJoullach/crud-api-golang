6c582e3f58425322af16e489ae0fa39d1d26066e
HutaJoullach <hutajoullach@gmail.com>
2023-03-21 22:45:05 +0900

    added  routes

--- main.go
+++ main.go
@@ -29,4 +29,10 @@ func main(){
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8080\n")
+	log.Fatal(http.LestenAndServer(":8080", r))
 }
